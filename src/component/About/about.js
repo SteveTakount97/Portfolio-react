48c1975136082e3dd5fe02ccb1d6d661d9623028
steve <stevetak@outlook.fr>
2024-09-11 17:15:22 +0200

    modif about

--- src/component/About/about.js
+++ src/component/About/about.js
@@ -7,7 +7,7 @@ const About = () => {
       <h2>À propos de moi</h2>
       <p className='about'>
       Avec un master en droit fiscal, j’ai exercé dans le domaine juridique avant de décider de suivre ma passion pour le développement web.
-       Cette reconversion professionnelle m’a permis d'acquérir des compétences solides en développement front-end et back-end. Je travaille principalement avec React,
+       Cette reconversion professionnelle m’a permis d'acquérir des compétences solides en développement ou l'intégration front-end des apllications modernes. Je travaille principalement avec React,
       JavaScript et des technologies modernes pour créer des expériences utilisateurs optimales.
       Mon parcours dans le droit m’a appris à aborder les projets avec une grande attention aux détails et une rigueur méthodologique, ce qui me sert désormais dans chaque projet web que je réalise.
       </p>
